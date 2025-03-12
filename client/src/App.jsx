2cc6fc20a44f234aadd28f71401e8fd097758d28
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2025-03-12 11:12:16 +0600

    Seperate the backend from frontend

--- client/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold text-center mt-10">
-      Im a mini shop
-    </h1>
-  )
-}
--- services/generalInformation/infoFetch.js
+++ services/generalInformation/infoFetch.js
@@ -1,6 +1,14 @@
 const generalInformation = require('../../models/generalInformation.model')
 
 const infoFetch = async () => {
+
+    try {
+        const info = await generalInformation.find()
+        return info
+    } catch (error) {
+        console.log(error)
+    }
+}
     
 
 module.exports = infoFetch
